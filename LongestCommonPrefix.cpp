77db1bd2ba0e6d438635b3268af0b8b53a3f68ed
Vimal M <msvimal10@gmail.com>
2021-08-19 22:17:19 +0530

    Sorting and conquer technique added

--- LongestCommonPrefix.cpp
+++ LongestCommonPrefix.cpp
@@ -46,3 +46,16 @@ int main()
     cout<<res<<endl;
     return 0;
 }
+
+/* Sorting the array and compare 1st and last element only  
+
+string longestCommonPrefix(vector<string>& strs) {
+	if( strs.size() == 0 ) return "";
+	sort(strs.begin(), strs.end());
+	string f = strs[0] , l = strs[strs.size()-1], result="";
+		int n = f.size(), m = l.size();
+		for(int i =0; i < n && i < m && f[i] == l[i]; i++)
+			result += f[i];
+	return result;
+}
+*/
