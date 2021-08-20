6e40a90e97ebea03101182a8b9004fddcda0dac7
Vimal M <msvimal10@gmail.com>
2021-08-20 14:26:52 +0530

    O(n) unordered set - little bit tricky but easy

--- LongSubstrWithoutRepeating.cpp
+++ LongSubstrWithoutRepeating.cpp
@@ -48,3 +48,51 @@ int main()
     cout<<res<<endl;
     return 0;
 }
+
+/*
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public: 
+    int lengthOfLongestSubstring(string s) {
+    int n=s.size();
+    int j=0;
+    int maxx=0;
+    int i=0;
+    unordered_set<int> st;
+    while(j<n)
+    {       
+        //char c=s[j]; 
+        if (st.find(s[j]) == st.end())// c not in st so add it
+        {
+        st.insert(s[j++]); // st=v and goes on and finally st=vimals 
+                           // then 'a' already in st hence insertion will not happen
+                           // cnt=6 
+        }
+        else
+            st.erase(s[i++]); //deleting all letters before 's' then st='s' and again cnting from 's'
+                              // st=sa and goes on and finally st=sa --> cnt-->2
+                              // then 'a' already in st hence deletion will not happen
+                              // check for max cnt (6,2) --> max=6
+                              // then again doing the same after 'a' i.e. deleting before 'a' 
+                              //then st='a' and again cnting from 'a' doing same 
+        
+        int stsize=st.size();
+        maxx= max(maxx,stsize);
+        
+    }
+    return maxx;
+     }
+};
+int main()
+{
+    
+    string s = "vimalsasi";
+    Solution ob;
+    int res=ob.lengthOfLongestSubstring(s);
+    cout<<res<<endl;
+    return 0;
+}
+*/
