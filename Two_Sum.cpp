5abfc2b2472258a2e04ad24334086aa6fb08d9ab
Vimal M <msvimal10@gmail.com>
2021-08-23 12:37:08 +0530

    Main method added with user-input case

--- Two_Sum.cpp
+++ Two_Sum.cpp
@@ -26,3 +26,28 @@ public:
         
     }
 };
+
+int main() {
+    
+    vector<int>v={2,9,6,4,5};
+    int target=7;
+    Solution s;
+    vector<int>ans=s.twoSum(v,target);
+    for(auto it:ans)
+        cout<<it<<" ";
+    return 0;
+    
+    /*
+    string input;
+    getline(cin,input);
+    istringstream is(input);
+    vector<int>v((istream_iterator<int>(is)),istream_iterator<int>() );
+    int target;
+    cin>>target;
+    Solution s;
+    vector<int>ans=s.twoSum(v,target);
+    for(auto it:ans)
+        cout<<it<<" ";
+    return 0;
+    */
+}
