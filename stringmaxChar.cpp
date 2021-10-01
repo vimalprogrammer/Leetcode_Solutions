a651e12a6f35de37341aef95c33112f8ca818552
Vimal M <msvimal10@gmail.com>
2021-10-01 20:40:08 +0530

    GFG - max Char without key value pair

--- stringmaxChar.cpp
+++ stringmaxChar.cpp
@@ -36,3 +36,34 @@ int main()
     cout<<ans;
     return 0;
 }
+
+//Hashing Technique not using map
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string maxChar(string s)
+{
+    int a[256]={0};
+    int max=0;
+    string st;
+    for(int i=0;i<s.size();i++)
+    {
+        a[s[i]]++;
+        if(a[s[i]]>max)
+        {
+            max=a[s[i]];
+            st=s[i];
+        }
+    }
+    return st;
+}
+
+int main()
+{
+    string s;
+    cin>>s;
+    string res=maxChar(s);
+    cout<<res;
+    return 0;
+}
